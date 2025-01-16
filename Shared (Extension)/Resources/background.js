690f4246ad16a19079832aa769cc3226e21d0968
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-01-16 08:54:23 +0530

    backup

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -4,3 +4,9 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.greeting === "hello")
         return Promise.resolve({ farewell: "goodbye" });
 });
+
+browser.browserAction.onClicked.addListener(() => {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    browser.tabs.executeScript(tabs[0].id, { file: "content.js" });
+  });
+});
--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -5,3 +5,18 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('running ataraxy 3333...')
+    const targetId = "shorts-container"; // Replace with your actual ID
+    const element = document.getElementById(targetId);
+    
+    if (element) {
+        element.style.display = "none";
+        console.log(`Element with ID '${targetId}' was hidden.`);
+    } else {
+        console.log(`Element with ID '${targetId}' not found.`);
+    }
+});
+
